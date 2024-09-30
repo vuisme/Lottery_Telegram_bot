33454cce1494d098e4b824be51096ad5ece36c8c
codemaster1022 <codemaster.han1022127@gmail.com>
2024-09-29 23:02:35 -0700

    message test

--- config/telegramBotHandler.js
+++ config/telegramBotHandler.js
@@ -1,1 +1,20 @@
 const axios = require('axios')
+const { bot } = require('./config'); // Import config
+
+class TelegramBotHandler {
+    constructor() {
+        this.bot = bot;
+        this.startMessage = 'Hey there, lottery master! 👑 \nThis bot is your one-stop shop for creating exciting Flash Lotteries. Use the following buttons to start building your next big draw!';
+    }
+    start() {
+        console.log("chat started!");
+        this.bot.onText(/\/start/, async(msg) => {
+            const chatId = msg.chat.id;
+            const username = msg.from.username;
+            console.log('test----------->', chatId, username);
+            bot.sendMessage(chatId, 'Hello World!');
+        })
+    }
+}
+
+module.exports = TelegramBotHandler;
--- server.js
+++ server.js
@@ -44,8 +44,12 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const pg = require('pg');
+const TelegramBotHandler = require('./config/telegramBotHandler');
 
 const channelId = '@lottery_ch';
 
+const botHandler = new TelegramBotHandler();
 const app = express();
 const port = process.env.PORT || 5001;
+
+botHandler.start();
