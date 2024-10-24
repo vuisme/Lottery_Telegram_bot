e3811675c8e28903692474462ebea430d5b8a3d4
techsmart1022 <techsmart1022127@gmail.com>
2024-10-24 17:49:38 +0700

    telegram message

--- config/telegramBotHandler.js
+++ config/telegramBotHandler.js
@@ -36,86 +36,86 @@ class TelegramBotHandler {
         });
 
         // Handle the /new_lottery command
-        this.bot.onText(/\/new_lottery/, async (msg) => {
-            const chatId = msg.chat.id;
-            const username = msg.from.username;
-            this.lotteries[chatId] = new Lottery(chatId, username);
-            this.lotteries[chatId].reset(chatId);
-            this.lotteries[chatId].sendMessage(this.startMessage, keyboards);
-            // Send the "hello world" message when /new_lottery is triggered
-            this.commandStatus[chatId] = 'new_lottery';
-        });
-        this.bot.on('callback_query', async (query) => {
-            const chatId = query.message.chat.id;
-            const data = query.data;
-            const lottery = this.lotteries[chatId];
+        // this.bot.onText(/\/new_lottery/, async (msg) => {
+        //     const chatId = msg.chat.id;
+        //     const username = msg.from.username;
+        //     this.lotteries[chatId] = new Lottery(chatId, username);
+        //     this.lotteries[chatId].reset(chatId);
+        //     this.lotteries[chatId].sendMessage(this.startMessage, keyboards);
+        //     // Send the "hello world" message when /new_lottery is triggered
+        //     this.commandStatus[chatId] = 'new_lottery';
+        // });
+        // this.bot.on('callback_query', async (query) => {
+        //     const chatId = query.message.chat.id;
+        //     const data = query.data;
+        //     const lottery = this.lotteries[chatId];
 
-            switch(data){
-                case 'TITLE':
-                    this.inputStatus[chatId] = 'TITLE',
-                    bot.sendMessage(chatId,"Input the title of this lottery!");
-                    break;
-                case "DESCR":
-                    this.inputStatus[chatId] = "DESCR",
-                    bot.sendMessage(chatId, "Input the description of this lottery");
-                    break;
-                case 'BUYLIMIT':
-                    this.inputStatus[chatId] = 'BUYLIMIT';
-                    bot.sendMessage(chatId, 'BuyLimit number must be integer like 5 or 10');
-                    break;
-                case 'ESTPRICE':
-                    this.inputStatus[chatId] = 'ESTPRICE';
-                    bot.sendMessage(chatId, 'Choose the payment method of this lottery', {
-                        reply_markup: JSON.stringify({
-                            inline_keyboard: [
-                                [
-                                    { text: 'USDT', callback_data: 'USDT' },
-                                    { text: 'TON', callback_data: 'TON' },
-                                    { text: 'ERC', callback_data: 'ERC' },
-                                    { text: 'SOL', callback_data: 'SOL' },
-                                ]
-                            ]
-                        })
-                    });
-                    break;
-                case 'RESET':
-                    if (this.lotteries[chatId]) {
-                        this.lotteries[chatId].reset();
-                        this.lotteries[chatId].sendMessage(this.startMessage, keyboards);
-                        this.inputStatus[chatId] = 'RESET';
-                    }
-                    break;
-                case 'CONFIRM':
-                    if (this.inputStatus[chatId] === 'CONFIRM') {
-                        this.bot.sendMessage(chatId, 'You have already published this lottery.');
-                    } else {
-                        if (this.lotteries[chatId]) {
-                            this.flag = "end_lottery"
-                            // this.commandStatus[chatId] = 'end_lottery';
-                            this.commandStatus[chatId] = 'fl';
-                            this.inputStatus[chatId] = 'CONFIRM';
-                            await this.lotteries[chatId].publishLottery();
-                        }
-                    }
-                    break;
-                case 'USDT':
-                    this.inputStatus[chatId] = 'USDT';
-                    bot.sendMessage(chatId, 'Insert the price of a single ticket in USDT');
-                    break;
-                case 'TON':
-                    this.inputStatus[chatId] = 'TON';
-                    bot.sendMessage(chatId, 'Insert the price of a single ticket in TON');
-                    break;
-                case 'ERC':
-                    this.inputStatus[chatId] = 'ERC';
-                    bot.sendMessage(chatId, 'Insert the price of a single ticket in ERC');
-                    break;
-                case 'SOL':
-                    this.inputStatus[chatId] = 'SOL';
-                    bot.sendMessage(chatId, 'Insert the price of a single ticket in SOL');
-                    break;
-            }
-        });
+        //     switch(data){
+        //         case 'TITLE':
+        //             this.inputStatus[chatId] = 'TITLE',
+        //             bot.sendMessage(chatId,"Input the title of this lottery!");
+        //             break;
+        //         case "DESCR":
+        //             this.inputStatus[chatId] = "DESCR",
+        //             bot.sendMessage(chatId, "Input the description of this lottery");
+        //             break;
+        //         case 'BUYLIMIT':
+        //             this.inputStatus[chatId] = 'BUYLIMIT';
+        //             bot.sendMessage(chatId, 'BuyLimit number must be integer like 5 or 10');
+        //             break;
+        //         case 'ESTPRICE':
+        //             this.inputStatus[chatId] = 'ESTPRICE';
+        //             bot.sendMessage(chatId, 'Choose the payment method of this lottery', {
+        //                 reply_markup: JSON.stringify({
+        //                     inline_keyboard: [
+        //                         [
+        //                             { text: 'USDT', callback_data: 'USDT' },
+        //                             { text: 'TON', callback_data: 'TON' },
+        //                             { text: 'ERC', callback_data: 'ERC' },
+        //                             { text: 'SOL', callback_data: 'SOL' },
+        //                         ]
+        //                     ]
+        //                 })
+        //             });
+        //             break;
+        //         case 'RESET':
+        //             if (this.lotteries[chatId]) {
+        //                 this.lotteries[chatId].reset();
+        //                 this.lotteries[chatId].sendMessage(this.startMessage, keyboards);
+        //                 this.inputStatus[chatId] = 'RESET';
+        //             }
+        //             break;
+        //         case 'CONFIRM':
+        //             if (this.inputStatus[chatId] === 'CONFIRM') {
+        //                 this.bot.sendMessage(chatId, 'You have already published this lottery.');
+        //             } else {
+        //                 if (this.lotteries[chatId]) {
+        //                     this.flag = "end_lottery"
+        //                     // this.commandStatus[chatId] = 'end_lottery';
+        //                     this.commandStatus[chatId] = 'fl';
+        //                     this.inputStatus[chatId] = 'CONFIRM';
+        //                     await this.lotteries[chatId].publishLottery();
+        //                 }
+        //             }
+        //             break;
+        //         case 'USDT':
+        //             this.inputStatus[chatId] = 'USDT';
+        //             bot.sendMessage(chatId, 'Insert the price of a single ticket in USDT');
+        //             break;
+        //         case 'TON':
+        //             this.inputStatus[chatId] = 'TON';
+        //             bot.sendMessage(chatId, 'Insert the price of a single ticket in TON');
+        //             break;
+        //         case 'ERC':
+        //             this.inputStatus[chatId] = 'ERC';
+        //             bot.sendMessage(chatId, 'Insert the price of a single ticket in ERC');
+        //             break;
+        //         case 'SOL':
+        //             this.inputStatus[chatId] = 'SOL';
+        //             bot.sendMessage(chatId, 'Insert the price of a single ticket in SOL');
+        //             break;
+        //     }
+        // });
         this.bot.on('message', async (msg) => {
             const chatId = msg.chat.id;
             const text = msg.text;
