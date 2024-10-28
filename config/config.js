04d732cea2d6cc9ec3ee8551237d3812f341a8ab
cpanel10x <me@vutn.net>
2024-10-28 15:14:41 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const token = "7703771063:AAG_xk0swI_kton1B1mBZKXSPQ9cpZ3qeNM"
+const token = process.env.TOKEN;
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(token, { polling: true });
 
